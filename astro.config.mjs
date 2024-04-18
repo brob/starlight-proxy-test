e57fec34d7f85bc491bc4882cbd16373a0dd1b5f
Bryan Robinson <bryanlrobinson@gmail.com>
2024-04-18 08:59:02 -0400

    removes the trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/docs/',
+	base: '/docs',
 	paths: {
 		"@images": "src/images/*",
 	},
